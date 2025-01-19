7959dfec2bea9223ab53d1f12c098206b44bf195
Ororura <hitmanlol890@gmail.com>
2025-01-19 12:50:52 +0300

    feat: add create review page

---
+++ src/features/createReview/model/type.ts
@@ -0,0 +1,10 @@
+type ReviewForm = {
+  title: string;
+  content: string;
+  rating: number;
+  image_path: string;
+  user_id: number;
+  category_id: number;
+};
+
+export type { ReviewForm };
---
+++ src/features/createReview/ui/CreateReview.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { ReviewForm } from "features/createReview/model";
+
+const CreateReview: FC = () => {
+  const onSubmit = async (data: ReviewForm) => {};
+  return <div></div>;
+};
+
+export { CreateReview };
