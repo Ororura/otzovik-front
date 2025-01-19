c081a5f4c1a08beff6b34a8785f83f6f01a64d13
Ororura <hitmanlol890@gmail.com>
2025-01-19 11:28:49 +0300

    feat: add header

---
+++ src/features/header/ui/Header.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import Link from "next/link";
+
+const Header: FC = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="max-w-screen-xl mx-auto flex justify-between">
+        <h1 className="text-2xl">Otzovik</h1>
+        <ul className="flex items-center space-x-4">
+          <li className="text-center">
+            <Link href={"/"} className="hover:underline">
+              Home
+            </Link>
+          </li>
+          <li className="text-center">
+            <Link href={"/"} className="hover:underline">
+              Create review
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export { Header };
